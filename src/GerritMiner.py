ce1d4a2494bc4b33affe85a02b8585766db7eda6
Filipe Falcão <filipefalcaosantos@gmail.com>
2016-04-20 14:12:33 -0300

    Gerrit-miner 0.3
    Now a CLI with options: mine local data, mine remote data and download
    remote data.

--- src/GerritMiner.py
+++ src/GerritMiner.py
@@ -1,17 +1,52 @@
-from Miner import Miner
+from LocalMiner import LocalMiner
+from RemoteMiner import RemoteMiner
+from argparse import ArgumentParser
+from os import path, makedirs, chdir
 
-if __name__ == '__main__':
-    domains = ['https://android-review.googlesource.com/', 'https://gerrit.chromium.org/gerrit/']
-    status = ['open', 'merged', 'abandoned']
+status = ['open', 'merged', 'abandoned']
 
-    print("Gerrit URLs to mine:")
 
-    for domain in domains:
-        print("\t" + domain)
+def download_data(args):
+    print("Gerrit domains to download data:")
+
+    for arg in args:
+        print('\t' + arg)
 
     print()
 
-    for domain in domains:
+    for arg in args:
+        domain_split = arg.split('//')[1]
+
+        if not path.exists(domain_split):
+            makedirs(domain_split)
+
+        chdir(domain_split)
+
         for stat in status:
-            print("Mining " + stat + " reviews from " + domain )
-            Miner(domain, stat)
+            print("Downloading data of " + stat + " reviews from " + arg)
+            LocalMiner(arg, stat)
+
+        chdir('../')
+
+
+def mine_local(args):
+    print(args)
+
+
+def mine_remote(args):
+    print(args)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--local', metavar='l', nargs='+', type=str, help='mine local data from domains list')
+    parser.add_argument('--remote', metavar='r', nargs='+', type=str, help='mine remote data from domains list')
+    parser.add_argument('--download', metavar='d', nargs='+', type=str, help='download data from domains list')
+
+    args = parser.parse_args()
+
+    if args.local:
+        mine_local(args.local)
+    elif args.remote:
+        mine_remote(args.remote)
+    elif args.download:
+        download_data(args.download)
